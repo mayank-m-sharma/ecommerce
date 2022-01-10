385ddf1061828df5c2c3fc7ebf1cbd3ce36c3f82
Mayank Sharma <mayanksharma@Mayanks-MacBook-Air.local>
2022-01-10 23:22:47 +0530

    day 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,43 @@
 import './App.css';
 import React, {useEffect, useState} from 'react'
+import { Product } from './components/product/Product';
+import { Nav } from './components/nav/Nav';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = ({products}) => {
+  return (
+    <div className="products-container"> 
+      {products.map(product => {
+        return <Product key={product._id} id={product._id} title={product.title} price={product.price} img={product.imgUrl}/>
+      })}
+    </div>
+  )
+}
 
 function App() {
-  const url = "https://jsonplaceholder.typicode.com/posts"
-  const url1 = "https://ecombackend1.herokuapp.com/api/product"
+  
+  const url1 = "http://localhost:8800/api/product"
   const [products, setProducts] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true); 
     useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch(url1, {
-        method: 'GET',
-        mode: 'cors',
-        headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-      });
+      const response = await fetch(url1);
       const data = await response.json()
-      console.log(data)
+      setProducts(data)
     }
     fetchProducts()
    },[])
   return (
-    <div className="App">
-        {/* {products.map(product => (
-          <h1>{product}</h1>
-        ))} */}
-        <p>hello world</p>
-    </div>
+   <Router>
+      <Routes>
+          <Route path="/" element={<Home products={products}/>} />
+      </Routes>
+   </Router>
   );
 }
 
